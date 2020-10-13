1ef7313ba2a79075756aba9e0d9b9498251143b9
Dempsey FP <dempseyfinley@gmail.com>
2020-10-13 14:44:44 -0400

    added some comments to JS

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// runs function right when page is loaded
 $(document).ready(function() {
 let date = moment().format("MMM " + "D " + "YYYY");
 
 console.log(date);
 $("#currentDay").append(date);
+// searches weatherAPI with city name with input value
 $("#citySearch").on("click", function(event){
     event.preventDefault();
     let city = $("#cityInput").val();
@@ -17,11 +19,15 @@ $("#citySearch").on("click", function(event){
         let lon = response.coord.lon;
         let cityName = response.name;
         console.log(response);
+        // uses lat and lon in response to do another ajax call for UV and 4 day forecast
         $.ajax({
+          // had to change "weather" to "onecall" in URL 
           url: "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=Imperial&appid="+apiKey,
           method: "GET"
+          // using "res" as shorthand for "response"
         }).then(function(res){
           console.log(res);
+          // creating divs dynamically
           let todayWeatherCard = $(`
             <div class="card">
             <h2>${cityName} (${date})</h2><img src="http://openweathermap.org/img/wn/${res.current.weather[0].icon}@4x.png"/>
